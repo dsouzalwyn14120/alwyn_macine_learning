341bb745cc64d2cdcee3e0076809790a87170702
Alwyn D Souza <alwyn14120@gmail.com>
2025-01-30 12:18:30 +0530

    Update streamlit_app.py
    modifications:
    
    change to indentation of the 2nd expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,6 @@ with st.expander("data"):
 
   # adding some data visualization to the same application
 
-  with  st.expander("**Data Visualization**"):
-    st.write("**Bill legth V/S Body mass**")
-    st.scatter_chart(data=df, x="bill_length_mm", y="body_mass_g", color ="species" )
+with  st.expander("**Data Visualization**"):
+  st.write("**Bill legth V/S Body mass**")
+  st.scatter_chart(data=df, x="bill_length_mm", y="body_mass_g", color ="species" )
