29323f527f6e677a5cc106b742090b05dcd41e0b
Alwyn D Souza <alwyn14120@gmail.com>
2025-01-30 17:53:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ with st.sidebar:
   island= st.selectbox("Island",('Torgersen' ,'Biscoe', 'Dream'))
   sex= st.selectbox("Sex",("male","female"))
   bill_length_mm= st.slider("Bill Length(mm)",int(df["bill_length_mm"].min()),int(df["bill_length_mm"].max()),int(df["bill_length_mm"].mean()))
-  bill_depth_mm= st.slider("Bill depth(mm)", df["bill_depth_mm"].min(), df["bill_depth_mm"].max(), df["bill_depth_mm"].min())
+  bill_depth_mm= st.slider("Bill depth(mm)", int(df["bill_depth_mm"].min()), int(df["bill_depth_mm"].max()),int( df["bill_depth_mm"].min()))
   flipper_length_mm= st.slider("Flipper length(mm)",df["flipper_length_mm"].min(), df["flipper_length_mm"].max(), int(df["flipper_length_mm"].mean()))
 
 
