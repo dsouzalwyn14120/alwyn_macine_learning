985c897b99b01fc1561c5a55b5c0cdad563e361a
Alwyn D Souza <alwyn14120@gmail.com>
2025-01-30 11:58:33 +0530

    Update streamlit_app.py
    modification:
    
    Importing the dataset, using the raw link and creation of Dataframe using Pandas.DataFrame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd 
+
+#reading the penguins_cleaned.csv file using the raw link
+df= pd.read_csv("https://raw.githubusercontent.com/dsouzalwyn14120/data/refs/heads/master/penguins_cleaned.csv")
 
 st.title('Machine learning app 🤖')
 
 st.info('in this app we build a Machine Learning model!') #this is used to print text, same as print()
+
+df
