ebc4e5642e093047801b05e9fb02a588949aa053
Alwyn D Souza <alwyn14120@gmail.com>
2025-01-30 18:25:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,10 @@ with st.sidebar:
 # for col in cols:
   # st.write(f"Column name:{col}\n\n {df[col].unique()}")
 
+#creating a dataframe for input features 
+  data={"island": island,"sex": sex,"bill_length_mm":bill_length_mm,"bill_depth_mm":bill_depth_mm,"flipper_length_mm":flipper_length_mm,
+      "body_mass_g": body_mass_g}
+  input_df= pd.DataFrame(data, index=[0]\
+input_df
+  
 
